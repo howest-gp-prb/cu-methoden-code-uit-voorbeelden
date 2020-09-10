346b2c205406c3b62fab1b84f4a6867c81e47192
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-10 15:04:49 +0200

    Add project files.

---
+++ BerekenSom.WPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BerekenSom.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int som;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int BerekenSommen(int getalLinks, int getalRechts)
+        {
+            return getalLinks + getalRechts;
+        }
+        int BerekenSommen(string getalLinks, string getalRechts)
+        {
+            int links = int.Parse(getalLinks);
+            int rechts = int.Parse(getalRechts);
+            return BerekenSommen(links, rechts);
+        }
+
+        private void btnBerekenSom_Click(object sender, RoutedEventArgs e)
+        {
+            int links = int.Parse(txtGetalLinks.Text);
+            int rechts = int.Parse(txtGetalRechts.Text);
+            som = BerekenSommen(links, rechts);
+            MessageBox.Show("De som van " + links + " en " + rechts + " is " + som, "Som berekenen");
+
+        }
+        void ScopeText()
+        {
+            MessageBox.Show("De som was: " + som);
+        }
+
+    }
+}
---
+++ BerekenTotaal.WPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BerekenTotaal.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBereken_Click(object sender, RoutedEventArgs e)
+        {
+            float tarief;
+            decimal prijsExcl;
+            tarief = float.Parse(txtTarief.Text);
+            prijsExcl = decimal.Parse(txtExcl.Text);
+            tbkBerekening.Text = BtwBerekeningTonen(tarief, prijsExcl);
+        }
+        decimal BtwBedragBerekenen(float tarief, decimal prijsExcl)
+        {
+            decimal btwBedrag;
+            btwBedrag = prijsExcl * (decimal)tarief / 100;
+            return btwBedrag;
+        }
+        string BtwBerekeningTonen(float tarief, decimal prijsExcl)
+        {
+            string samenvatting = "";
+            decimal btwBedrag;
+            decimal prijsIncl;
+            btwBedrag = BtwBedragBerekenen(tarief, prijsExcl);
+            prijsIncl = prijsExcl + btwBedrag;
+            samenvatting = "Berekening BTW\n\n" +
+                $"BTW excl.\t€ {prijsExcl.ToString("0.00")}\n" +
+                $"BTW tarief\t{tarief} %\n" +
+                $"BTW bedrag\t€ {btwBedrag.ToString("0.00")}\n" +
+                $"BTW incl\t\t€ {prijsIncl.ToString("0.00")}\n";
+            return samenvatting;
+        }
+
+
+
+    }
+}
